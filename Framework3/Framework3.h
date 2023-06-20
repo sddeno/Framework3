f4384fcb51f9aefceda30f4175c7496cf1f29510
Shubham Deshmukh <shubhamdeshmukh69@gmail.com>
2023-06-20 16:35:15 +0530

    Initial Commit

---
+++ Framework3/Framework3.h
@@ -0,0 +1,18 @@
+//
+//  Framework3.h
+//  Framework3
+//
+//  Created by Shubham Deshmukh on 20/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Framework3.
+FOUNDATION_EXPORT double Framework3VersionNumber;
+
+//! Project version string for Framework3.
+FOUNDATION_EXPORT const unsigned char Framework3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework3/PublicHeader.h>
+
+
